10dbdb59ac7dd03153f26780d2845b5a401db896
Maggie Maldjian <maggiemaldjian@gmail.com>
2020-07-16 16:14:03 -0400

    Project 6b pre-challenge

--- Project6b/Project6b/ViewController.swift
+++ Project6b/Project6b/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-
+    
     let label1 = UILabel()
     label1.translatesAutoresizingMaskIntoConstraints = false
     label1.backgroundColor = UIColor.red
@@ -48,15 +48,23 @@ class ViewController: UIViewController {
     view.addSubview(label3)
     view.addSubview(label4)
     view.addSubview(label5)
-    
-    let viewsDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
 
+    var previous: UILabel?
+    
     for label in [label1, label2, label3, label4, label5] {
       label.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
       label.heightAnchor.constraint(equalToConstant: 88).isActive = true
+      
+      if let previous = previous {
+        label.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 10).isActive = true
+      } else {
+        label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+      }
+      previous = label
     }
     /*
      let metrics = ["labelHeight": 88]
+     let viewsDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
      
      for label in viewsDictionary.keys {
         view.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
@@ -64,6 +72,17 @@ class ViewController: UIViewController {
     view.addConstraints(
         NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]->=10-|", options: [], metrics: metrics, views: viewsDictionary))
   }
+     
+       label.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+       label.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+         label.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 10).isActive = true
+         label.leadingAnchor.constraint(equalTo: previous.leadingAnchor, constant: 3).isActive = true
+         label.trailingAnchor.constraint(equalTo: previous.trailingAnchor, constant: 3).isActive = true
+         label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 1).isActive = true
+         label.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 1).isActive = true
+       }
+       previous = label
+     }
  */
   }
 }
