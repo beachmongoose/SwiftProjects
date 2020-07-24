95eddd2ba7f7594c9b3a4029833ed1786eb1603c
Maggie Maldjian <maggiemaldjian@gmail.com>
2020-07-24 14:09:03 -0400

    adding Project 12a and 12b

---
+++ Project12 prj1/Project1edit/ImageCell.swift
@@ -0,0 +1,15 @@
+//
+//  ImageCell.swift
+//  Project1edit
+//
+//  Created by Maggie Maldjian on 7/23/20.
+//  Copyright © 2020 Maggie Maldjian. All rights reserved.
+//
+
+import UIKit
+
+class ImageCell: UICollectionViewCell {
+  @IBOutlet var image: UIImageView!
+  @IBOutlet var name: UILabel!
+  
+}
