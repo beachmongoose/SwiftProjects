18470833e1a3501c77b08dc7e1c133bf556a6b1d
Maggie Maldjian <maggiemaldjian@gmail.com>
2020-08-07 12:41:56 -0400

    Adding Project 19

---
+++ Project19/Extension/ActionViewController.swift
@@ -0,0 +1,91 @@
+//
+//  ActionViewController.swift
+//  Extension
+//
+//  Created by Maggie Maldjian on 8/7/20.
+//  Copyright © 2020 Maggie Maldjian. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ActionViewController: UIViewController {
+  @IBOutlet var script: UITextView!
+  var pageTitle = ""
+  var pageURL = ""
+  
+    override func viewDidLoad() {
+      
+      navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+        super.viewDidLoad()
+      navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Defaults", style: .plain, target: self, action: #selector(defaultsMenu))
+      
+      let notificationCenter = NotificationCenter.default
+      notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+      notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+      
+      if let inputItem = extensionContext?.inputItems.first as? NSExtensionItem {
+        if let itemProvider = inputItem.attachments?.first {
+          itemProvider.loadItem(forTypeIdentifier: kUTTypePropertyList as String) { [weak self] (dict, error) in
+            guard let itemDictionary = dict as? NSDictionary else { return }
+            guard let javaScriptValues = itemDictionary[NSExtensionJavaScriptPreprocessingResultsKey] as? NSDictionary else { return }
+            self?.pageTitle = javaScriptValues["title"] as? String ?? ""
+            self?.pageURL = javaScriptValues["URL"] as? String ?? ""
+            
+            DispatchQueue.main.async {
+              self?.title = self?.pageTitle
+            }
+          }
+        }
+      }
+    }
+}
+
+extension ActionViewController {
+  
+  @objc func adjustForKeyboard(notification: Notification) {
+    guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+    
+    let keyboardScreenEndFrame = keyboardValue.cgRectValue
+    let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+    
+    if notification.name == UIResponder.keyboardWillHideNotification {
+      script.contentInset = .zero
+      } else {
+      script.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
+    }
+    
+    script.scrollIndicatorInsets = script.contentInset
+    
+    let selectedRange = script.selectedRange
+    script.scrollRangeToVisible(selectedRange)
+  }
+  
+    @IBAction func done() {
+      let item = NSExtensionItem()
+      let argument: NSDictionary = ["customJavaScript": script.text!]
+      let webDictionary: NSDictionary = [NSExtensionJavaScriptFinalizeArgumentKey: argument]
+      let customJavaScript = NSItemProvider(item: webDictionary, typeIdentifier: kUTTypePropertyList as String)
+      item.attachments = [customJavaScript]
+      
+      extensionContext?.completeRequest(returningItems: [item])
+    }
+
+  @objc func defaultsMenu() {
+    let defaultsList = UIAlertController(title: "Select Default Code", message: nil, preferredStyle: .alert)
+    defaultsList.addAction(UIAlertAction(title: "Alert", style: .default, handler: { action in
+      self.changeScriptText(to: "alert(document.title);")
+    }))
+    defaultsList.addAction(UIAlertAction(title: "Test", style: .default, handler: { action in
+      self.changeScriptText(to: "Bluh")
+    }))
+    defaultsList.addAction(UIAlertAction(title: "Test2", style: .default, handler: { action in
+      self.changeScriptText(to: "Bluh")
+    }))
+    defaultsList.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+    present(defaultsList, animated: true)
+  }
+  func changeScriptText(to selectedDefault: String) {
+  self.script.text = selectedDefault
+  }
+}
